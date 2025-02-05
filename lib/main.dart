5486f6a1e7d3dee2fd0699adee2c834ccdecb091
mylanewby <167246119+mylanewby@users.noreply.github.com>
2025-02-05 18:21:53 -0500

    Displays weather information

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-  
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -27,6 +41,16 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
   final String title;
 
   @override
@@ -42,6 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   /*void _incrementCounter() {
     setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }*/
@@ -51,8 +80,6 @@ class _MyHomePageState extends State<MyHomePage> {
       _cityName = text1.text;
       _temperature = Random().nextInt(15) + 15;
       _condition = weatherOptions[Random().nextInt(weatherOptions.length)];
-      print(_cityName + '$_temperature' + _condition);
-      //Text('$_condition');     
     });
   }
 
@@ -76,7 +103,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 controller: text1,
             ),
-            //Insert display info here once simulation is complete
+            Text(
+              _cityName, 
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              '$_temperature',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text(
+              _condition,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
             ElevatedButton(
               onPressed: _fetchWeather,
               child: const Text('Fetch Weather'))
