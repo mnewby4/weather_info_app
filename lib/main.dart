cf1757fb666ca29a264201a70e21d243f27e0178
mylanewby <167246119+mylanewby@users.noreply.github.com>
2025-02-05 18:41:23 -0500

    Tests and tweaks finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+
+//Reads the user's input
 TextEditingController text1 = TextEditingController ();
 
 void main() {
@@ -16,41 +17,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Weather App Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -58,23 +34,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  //Initializes the weather information
   String _cityName = "";
   int _temperature = 0;
   String _condition = "";
   List<String> weatherOptions = ["sunny", "cloudy", "rainy"];
 
-  /*void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }*/
-
+  //Sets the weather information
   void _fetchWeather() {
     setState(() {
       _cityName = text1.text;
@@ -93,8 +59,8 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          
           children: <Widget>[
+            //Input cityName
             TextField(
                 obscureText: false,
                 decoration: InputDecoration(
@@ -103,29 +69,18 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 controller: text1,
             ),
+            //Displays the weather data
             Text(
-              _cityName, 
+              '\n' + _cityName + '\n\n' + '$_temperature' + '\n\n' + _condition + '\n', 
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-            Text(
-              '$_temperature',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              _condition,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            //Fetch Weather data button
             ElevatedButton(
               onPressed: _fetchWeather,
               child: const Text('Fetch Weather'))
           ],
         ),
       ),
-      /*floatingActionButton: FloatingActionButton(
-        onPressed: _fetchWeather,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.*/
     );
   }
 }
